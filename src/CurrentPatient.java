cc58eb569810bde554f16709e345770d516fbaf7
whitney <whitneyvantassell@gmail.com>
2024-01-19 11:04:49 -0700

    adding CurrentPatient.java

---
+++ src/CurrentPatient.java
@@ -0,0 +1,28 @@
+package Assignment02;
+
+import java.util.GregorianCalendar;
+
+public class CurrentPatient extends Patient
+{
+    public CurrentPatient(String firstName, String lastName, UHealthID uHealthID, int physician, GregorianCalendar lastVisit)
+    {
+        super(firstName, lastName, uHealthID);
+
+    }
+    public int getPhysician()
+    {
+
+    }
+    public GregorianCalendar getLastVisit()
+    {
+
+    }
+    public void updatePhysician(int newPhysician)
+    {
+
+    }
+    public void updateLastVisit(GregorianCalendar date)
+    {
+
+    }
+}
